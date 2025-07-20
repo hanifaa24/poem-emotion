05bea135c1e483b5c44f9a6b09178a427eb0738f
hanifaa24 <hanifaabny05@gmail.com>
2025-07-20 09:59:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def predict():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Gunakan port dari Railway
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
